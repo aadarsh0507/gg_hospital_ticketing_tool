7f72ca6e7570af6b8e30d67eadabc349c69427ef
aadarsh0507 <infor0507@gmail.com>
2026-02-16 15:59:58 +0530

    app creating

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.9v6b73s4spc"
+    "revision": "0.eq7em30ap1s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
